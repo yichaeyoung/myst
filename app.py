d67da0f3bfcc58de3ff78a66efdaf086a40405b9
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-05-16 12:46:06 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("this is the app title")
+st.markdown("this is the markdown")
+st.header("this is the header")
+st.subheader("this is the subheader")
+std.caption("this is the caption")
+st.code("x=2021")
+st.latex(r'''
+a + a r ^1+ a r^2 + a r^3 ''')
