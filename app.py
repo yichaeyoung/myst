f5686b4a30545e918fb476ffe623755ea5ad1ce5
yichaeyoung <126742382+yichaeyoung@users.noreply.github.com>
2023-05-16 12:47:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title("this is the app title")
 st.markdown("this is the markdown")
 st.header("this is the header")
 st.subheader("this is the subheader")
-std.caption("this is the caption")
+st.caption("this is the caption")
 st.code("x=2021")
 st.latex(r'''
 a + a r ^1+ a r^2 + a r^3 ''')
